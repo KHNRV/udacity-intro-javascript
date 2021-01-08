84fdfe95d57ca29d9409f779e2b49a782988155b
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-01-08 20:36:58 +0000

    minor fix

--- Quiz: 99 Bottles of Juice (4-2).js
+++ Quiz: 99 Bottles of Juice (4-2).js
@@ -23,20 +23,13 @@
 var num = 99;
 
 while (num >= 1) {
-    if (num > 1) {
+    if (num > 2) {
       console.log(num + ' bottles of juice on the wall! ' + num + ' bottles of juice! Take one down, pass it around... ' + (num - 1) + ' bottles of juice on the wall!');
     } else if (num === 2) {
-      console.log(num + ' bottle of juice on the wall! ' + num + ' bottle of juice! Take one down, pass it around... ' + (num - 1) + ' bottle of juice on the wall!');
+      console.log(num + ' bottles of juice on the wall! ' + num + ' bottles of juice! Take one down, pass it around... ' + (num - 1) + ' bottle of juice on the wall!');
     } else if (num === 1) {
       console.log(num + ' bottle of juice on the wall! ' + num + ' bottle of juice! Take one down, pass it around... ' + (num - 1) + ' bottles of juice on the wall!');
     }
 
     num -= 1
 }
-
-99 bottles of juice on the wall! 99 bottles of juice! Take one down, pass it around... 98 bottles of juice on the wall!
-99 bottles of juice on the wall! 99 bottles of juice! Take one down, pass it around... 98 bottles of juice on the wall!
-
-1 bottle of juice on the wall! 1 bottle of juice! Take one down, pass it around... 0 bottles of juice on the wall!
-1 bottle of juice on the wall! 1 bottle of juice! Take one down, pass it around... 0 bottles of juice on the wall!
-
